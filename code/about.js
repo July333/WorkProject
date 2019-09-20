61ce762b501cd88cca0c04211badaf091aa1c664
july333 <yulia.ivanov359@gmail.com>
2019-09-20 17:43:19 +0300

    Finish

--- code/about.js
+++ code/about.js
@@ -1,5 +1,4 @@
 $("#about").on('click', function () {
-    console.log('hi');
     $("#chartContainer").css("visibility", "hidden");
     $("section").css("visibility", "hidden");
     $("section").css("position", "relative");
